d86aede3a6127317f313fc3206e40f25301ada16
Szymon Nowakowski <szymon.nowakowski@gmail.com>
2023-04-03 10:08:08 +0200

    added a docker script to run in local non-SLURM docker

---
+++ slurm/run_docker_multimer.sh
@@ -0,0 +1,17 @@
+
+#set the environment PATH
+export PYTHONNOUSERSITE=True
+#module load singularity
+BASE_DIR=$(pwd)
+
+
+#Run the command
+python3 alphafold/docker/run_docker.py \
+ --fasta_paths=$BASE_DIR/$1 \
+ --data_dir=/data_hdd/szym/data-2.3.1/ \
+ --output_dir=$BASE_DIR/outputs \
+ --model_preset=multimer \
+ --db_preset=full_dbs \
+ --docker_image_name=alphafold:2.3.1 \
+ --max_template_date=2022-12-31 
+
