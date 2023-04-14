f5ff45bfb39ffc96dcb78a31cdbec380f34b2d53
Szymon Nowakowski <szymon.nowakowski@gmail.com>
2023-04-14 09:09:16 +0200

    alphafold monomer running in docker

--- slurm/run_docker_alphafold.sh
+++ slurm/run_docker_alphafold.sh
@@ -10,7 +10,7 @@ python3 alphafold/docker/run_docker.py \
  --fasta_paths=$BASE_DIR/$1 \
  --data_dir=/data_hdd/szym/data-2.3.1/ \
  --output_dir=$BASE_DIR/outputs \
- --model_preset=multimer \
+ --model_preset=monomer \
  --db_preset=full_dbs \
  --docker_image_name=alphafold:2.3.1 \
  --max_template_date=2022-12-31 \
